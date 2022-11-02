9d6da8075a9d1b991a72e344b48ba58c9270b348
Adit Tanu <adiettanu27@gmail.com>
2022-11-02 16:42:22 +0700

    handle NaN feedback stats

--- src/components/FeedbackStats.jsx
+++ src/components/FeedbackStats.jsx
@@ -4,7 +4,7 @@ function FeedbackStats({ feedback }) {
         return prev + curr.rating
     }, 0) / feedback.length
 
-    average = average.toFixed(1).replace('/[.,]0$/', '')
+    average = average.toFixed(1).replace(/[.,]0$/, '')
 
     return (
         <div className='feedback-stats'>
