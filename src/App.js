6e9b3359cbc8d40cc380040f08579188575eec87
Adit Tanu <adiettanu27@gmail.com>
2022-11-02 16:38:36 +0700

    feedback stats

--- src/App.js
+++ src/App.js
@@ -2,14 +2,15 @@ import Header from './components/Header'
 import FeedbackItems from './components/FeedbackItems';
 import { useState } from 'react';
 import FeedbackData from './data/FeedbackData';
+import FeedbackStats from './components/FeedbackStats';
 
 function App() {
     const [feedback, setFeedback] = useState(FeedbackData)
 
     const deleteFeedback = (id) => {
-        setFeedback(feedback.filter((item) => {
-            return item.id !== id
-        }))
+        if(window.confirm("Are you sure want to delete?")) {
+            setFeedback(feedback.filter(item => item.id !== id))
+        }
     }
 
     const loading = false;
@@ -19,6 +20,7 @@ function App() {
         <>
             <Header />  
             <div className="container">
+                <FeedbackStats feedback={feedback}/>
                 <FeedbackItems feedback={feedback} handleDelete={deleteFeedback}/>
             </div>
         </>
---
+++ src/components/FeedbackStats.jsx
@@ -0,0 +1,27 @@
+import PropTypes from 'prop-types'
+function FeedbackStats({ feedback }) {
+    let average = feedback.reduce((prev, curr) => {
+        return prev + curr.rating
+    }, 0) / feedback.length
+
+    average = average.toFixed(1).replace('/[.,]0$/', '')
+
+    return (
+        <div className='feedback-stats'>
+            <h4>{feedback.length} Reviews</h4>
+            <h4>Average Rating : {average}</h4>
+        </div>
+    )
+}
+
+FeedbackStats.propTypes = {
+    feedback : PropTypes.arrayOf(
+        PropTypes.shape({
+            id: PropTypes.number.isRequired,
+            text: PropTypes.string.isRequired,
+            rating: PropTypes.number.isRequired
+        })
+    )
+}
+
+export default FeedbackStats
